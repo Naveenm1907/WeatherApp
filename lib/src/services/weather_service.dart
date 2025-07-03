901f473e5c9b6ee65ae639f5d7343db15c778e81
Naveenm1907 <naveenthakur1907@gmail.com>
2025-07-03 18:54:03 +0530

    api request

---
+++ lib/src/services/weather_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/weather_model.dart';
+
+class WeatherService {
+  static const String _baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService({required this.apiKey});
+
+  Future<WeatherModel> getWeather(String city) async {
+    final response = await http.get(
+      Uri.parse('$_baseUrl?q=$city&appid=$apiKey&units=metric'),
+    );
+
+    if (response.statusCode == 200) {
+      return WeatherModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
